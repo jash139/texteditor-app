5c7d9096814978e5e32eb8f9c31594483a762505
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-07 20:39:04 +0530

    Changed styling

--- src/components/TextEditor/TextEditor.js
+++ src/components/TextEditor/TextEditor.js
@@ -3,70 +3,21 @@ import React from "react";
 import "./TextEditor.css";
 
 function TextEditor() {
-    // var btn = document.querySelector(".sai");
-    // var getText = document.querySelector(".getText");
-    // var content = document.querySelector(".getcontent");
-    // var editorContent = document.querySelector(".editor");
-
-    // btn.addEventListener("click", function () {
-    //     var s = editorContent.innerHTML;
-    //     content.style.display = "block";
-    //     content.textContent = s;
-    // });
-
-    // getText.addEventListener("click", function () {
-    //     const old = editorContent.textContent;
-    //     content.style.display = "block";
-    //     content.textContent = old;
-    // });
-
-    function link() {
+    const addLink = () => {
         var url = prompt("Enter the URL");
         document.execCommand("createLink", false, url);
     }
 
-    // function copy() {
-    //     document.execCommand("copy", false, "");
-    // }
-
-    // function changeColor() {
-    //     var color = prompt("Enter your color in hex ex:#f1f233");
-    //     document.execCommand("foreColor", false, color);
-    // }
-
-
-    // function getImage() {
-    //     var file = document.querySelector("input[type=file]").files[0];
-
-    //     var reader = new FileReader();
-
-    //     let dataURI;
-
-    //     reader.addEventListener(
-    //         "load",
-    //         function () {
-    //             dataURI = reader.result;
-
-    //             const img = document.createElement("img");
-    //             img.src = dataURI;
-    //             editorContent.appendChild(img);
-    //         },
-    //         false
-    //     );
-
-    //     if (file) {
-    //         console.log("s");
-    //         reader.readAsDataURL(file);
-    //     }
-    // }
+    const printText = () => {
+        var editorContent = document.querySelector(".text-editor");
+        console.log(editorContent.textContent);
+    };
 
     const printInnerHtml = () => {
         var getContent = document.querySelector(".getcontent");
         var editorContent = document.querySelector(".text-editor");
-        getContent.textContent = editorContent.innerHTML;
-
-        console.log(editorContent);
-    }
+        console.log(editorContent.innerHTML);
+    };
 
     return (
         <React.Fragment>
@@ -86,7 +37,7 @@ function TextEditor() {
                 />
                 <button
                     className="tool-items fa fa-link"
-                    onClick={() => link()}
+                    onClick={() => addLink()}
                 />
                 <button
                     className="tool-items fa fa-undo"
@@ -113,11 +64,8 @@ function TextEditor() {
                 <p>Your content goes here</p>
             </div>
 
-            <button className="sai btn">GetHtml</button>
-            <button className="getText btn">GetText</button>
-            <button className="btn print" onClick={printInnerHtml}>PrintHtml</button>
-
-            <section className="getcontent" />
+            <button className="print-button" onClick={printText}>Print Text</button>
+            <button className="print-button" onClick={printInnerHtml}>Print Html</button>
         </React.Fragment>
     );
 }
